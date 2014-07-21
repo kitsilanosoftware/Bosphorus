c54d5d6c15388920cbdcb6a7c084753997825259
Damien Diederen <dd@crosstwine.com>
2014-07-21 18:50:11 +0200

    Loader: Trivial deserializer, from command-line argument to YamlStream

---
+++ Disunity.Loader/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using YamlDotNet.Core;
+using YamlDotNet.RepresentationModel;
+
+namespace Disunity.Loader
+{
+	public class Loader
+	{
+		public Loader ()
+		{
+		}
+
+		public static void Main (string[] args)
+		{
+			var stream = File.OpenText (args [0]);
+
+			var yaml = new YamlStream();
+			yaml.Load(stream);
+
+			Console.WriteLine ("Done, loaded {0} documents.", yaml.Documents.Count);
+		}
+	}
+}
