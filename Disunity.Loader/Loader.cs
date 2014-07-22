e6249a0f6e870dcba6f988b52cfa9d4c8ea2a6f0
Damien Diederen <dd@crosstwine.com>
2014-07-22 16:53:55 +0200

    Loader: Introduce Color class based on Unity's public interface

--- Disunity.Loader/Loader.cs
+++ Disunity.Loader/Loader.cs
@@ -42,9 +42,72 @@ namespace Disunity.Loader
 		}
 	}
 
+	public struct Color {
+
+		public Color (float r, float g, float b, float a)
+		{
+			this.r = r;
+			this.g = g;
+			this.b = b;
+			this.a = a;
+		}
+
+		public Color (float r, float g, float b)
+		{
+			this.r = r;
+			this.g = g;
+			this.b = b;
+			this.a = 1.0f;
+		}
+
+		// public static Color Lerp (Color a, Color b, float t);
+		// public override bool Equals (object other);
+		// public override int GetHashCode ();
+
+		public override string ToString ()
+		{
+			return String.Format ("RGBA({0}, {1}, {2}, {3})", r, g, b, a);
+		}
+
+		// public string ToString (string format);
+
+		// public static Color operator + (Color a, Color b);
+		// public static Color operator - (Color a, Color b);
+		// public static Color operator * (Color a, Color b);
+		// public static Color operator * (Color a, float b);
+		// public static Color operator * (float b, Color a);
+		// public static Color operator / (Color a, float b);
+		// public static bool operator == (Color lhs, Color rhs);
+		// public static bool operator != (Color lhs, Color rhs);
+		// public static implicit operator Vector4 (Color c);
+		// public static implicit operator Color (Vector4 v);
+
+		// public static Color black { get; }
+		// public static Color blue { get; }
+		// public static Color clear { get; }
+		// public static Color cyan { get; }
+		// public static Color gray { get; }
+		// public static Color green { get; }
+		// public static Color grey { get; }
+		// public static Color magenta { get; }
+		// public static Color red { get; }
+		// public static Color white { get; }
+		// public static Color yellow { get; }
+		// public Color gamma { get; }
+		// public float grayscale { get; }
+		// public float this [int index] { get; set; }
+		// public Color linear { get; }
+
+		public float r;
+		public float g;
+		public float b;
+		public float a;
+	}
+
 	public class RenderSettings
 	{
-		// public static Color ambientLight { get; set; }
+		[YamlAlias("m_AmbientLight")]
+		public /* static */ Color ambientLight { get; set; }
 
 		[YamlAlias("m_FlareFadeSpeed")]
 		public /* static */ float flareFadeSpeed { get; set; }
@@ -55,7 +118,8 @@ namespace Disunity.Loader
 		[YamlAlias("m_Fog")]
 		public /* static */ bool fog { get; set; }
 
-		// public static Color fogColor { get; set; }
+		[YamlAlias("m_FogColor")]
+		public /* static */ Color fogColor { get; set; }
 
 		[YamlAlias("m_FogDensity")]
 		public /* static */ float fogDensity { get; set; }
