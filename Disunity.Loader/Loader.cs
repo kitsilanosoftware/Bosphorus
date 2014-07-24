7f090edcb6426ba87d6c278f50fd8a5c5f11225f
Damien Diederen <dd@crosstwine.com>
2014-07-24 16:10:37 +0200

    Loader: Loop over all documents in stream; dispatch or skip via tag

--- Disunity.Loader/Loader.cs
+++ Disunity.Loader/Loader.cs
@@ -8,10 +8,14 @@ using YamlDotNet.Serialization.TypeResolvers;
 using YamlDotNet.Serialization.TypeInspectors;
 using YamlDotNet.Serialization.NamingConventions;
 using Disunity.UnityEngine;
+using System.Collections.Generic;
+using YamlDotNet.Serialization.Utilities;
+using YamlDotNet.Serialization.ObjectFactories;
+using System.Runtime.Serialization;
 
 namespace Disunity.Loader
 {
-	public class BoolConverter : IYamlTypeConverter
+	public sealed class BoolConverter : IYamlTypeConverter
 	{
 		public bool Accepts (Type type)
 		{
@@ -46,61 +50,183 @@ namespace Disunity.Loader
 		}
 	}
 
-	public class Top
+	public sealed class UnityNodeDeserializer : INodeDeserializer
 	{
-		public RenderSettings RenderSettings { get; set; }
+		private readonly IObjectFactory _objectFactory;
+		private readonly ITypeInspector _typeDescriptor;
+		private readonly bool _ignoreUnmatched;
+
+		public UnityNodeDeserializer(IObjectFactory objectFactory, ITypeInspector typeDescriptor, bool ignoreUnmatched)
+		{
+			_objectFactory = objectFactory;
+			_typeDescriptor = typeDescriptor;
+			_ignoreUnmatched = ignoreUnmatched;
+		}
+
+		private void CheckClassName (string className, Type expectedType)
+		{
+			if (expectedType.Name != className)
+			{
+				throw new SerializationException(
+					string.Format(
+						"Key name {0} does not match type {1}", className, expectedType
+					)
+				);
+			}
+		}
+
+		bool INodeDeserializer.Deserialize(EventReader reader, Type expectedType,
+						   Func<EventReader, Type, object> nestedObjectDeserializer, out object value)
+		{
+			if (!expectedType.IsSubclassOf(typeof(Disunity.UnityEngine.Object)))
+			{
+				value = null;
+				return false;
+			}
+
+			// Position: before the RenderSettings mapping in:
+			//
+			//     --- !u!104 &2
+			//     RenderSettings:
+			//       m_Fog: 1
+			//       ...
+			var mapping = reader.Allow<MappingStart>();
+			if (mapping == null)
+			{
+				value = null;
+				return false;
+			}
+
+			CheckClassName(reader.Expect<Scalar>().Value, expectedType);
+			value = _objectFactory.Create(expectedType);
+
+			// Position: after RenderSettings; now taking care of the real property
+			// mapping.
+			reader.Expect<MappingStart>();
+			while (!reader.Accept<MappingEnd>())
+			{
+				var propertyName = reader.Expect<Scalar>();
+				var property = _typeDescriptor.GetProperty(expectedType, null, propertyName.Value,
+									   _ignoreUnmatched);
+				if (property == null)
+				{
+					reader.SkipThisAndNestedEvents();
+					continue;
+				}
+
+				var propertyValue = nestedObjectDeserializer(reader, property.Type);
+				var propertyValuePromise = propertyValue as IValuePromise;
+				if (propertyValuePromise == null)
+				{
+					var convertedValue = TypeConverter.ChangeType(propertyValue, property.Type);
+					property.Write(value, convertedValue);
+				}
+				else
+				{
+					var valueRef = value;
+					propertyValuePromise.ValueAvailable += v =>
+					{
+						var convertedValue = TypeConverter.ChangeType(v, property.Type);
+						property.Write(valueRef, convertedValue);
+					};
+				}
+			}
+			// Double mapping.
+			reader.Expect<MappingEnd>();
+			reader.Expect<MappingEnd>();
+
+			return true;
+		}
 	}
 
 	public class Loader
 	{
+		public const string UnityPrefix = "tag:unity3d.com,2011:";
+
+		private Dictionary<string,Type> tagMapping = new Dictionary<string,Type>();
+
 		public Loader ()
 		{
+			tagMapping.Add(UnityPrefix + "104", typeof(RenderSettings));
 		}
 
-		public static void Main (string[] args)
+		private Deserializer CreateDeserializer ()
 		{
-			var stream = File.OpenText (args [0]);
-			var reader = new EventReader (new Parser (stream));
-			var deserializer = new Deserializer (ignoreUnmatched: true);
-
-			deserializer.RegisterTagMapping ("tag:unity3d.com,2011:104", typeof (Top));
-			deserializer.RegisterTypeConverter (new BoolConverter ());
+			var ignoreUnmatched = true;
+			var objectFactory = new DefaultObjectFactory();
+			var namingConvention = new NullNamingConvention();
+			var deserializer = new Deserializer(objectFactory, namingConvention, ignoreUnmatched);
 
 			var typeResolver = new StaticTypeResolver();
-
-			deserializer.TypeDescriptor =
+			var typeDescriptor =
 				new YamlAttributesTypeInspector(
 					new NamingConventionTypeInspector(
 						new ReadableAndWritablePropertiesTypeInspector(
 							new SelectManyTypeInspector(
 								new ReadablePropertiesTypeInspector(typeResolver),
 								new FieldsTypeInspector(typeResolver)
-							)
-						),
+								)
+							),
 						new NullNamingConvention()
-					)
-				);
+						)
+					);
 
-			reader.Allow<StreamStart> ();
-			if (reader.Accept<DocumentStart> ())
+			deserializer.TypeDescriptor = typeDescriptor;
+
+			var nodeDeserializers = deserializer.NodeDeserializers;
+			// Add our node deserializer just before the
+			// generic mapping -> object one.
+			nodeDeserializers.Insert(nodeDeserializers.Count - 1,
+				new UnityNodeDeserializer(objectFactory, typeDescriptor, ignoreUnmatched));
+
+			deserializer.RegisterTypeConverter (new BoolConverter ());
+
+			foreach (var pair in tagMapping)
 			{
-				// Skip SceneSettings as we don't know
-				// which class to map this to.
-				reader.SkipThisAndNestedEvents ();
+				deserializer.RegisterTagMapping(pair.Key, pair.Value);
 			}
 
-			var something = deserializer.Deserialize<Top> (reader);
+			return deserializer;
+		}
 
-			Console.WriteLine ("Deserialized {0}.", something);
-			Console.WriteLine ("\tambientLight: {0}", something.RenderSettings.ambientLight);
-			Console.WriteLine ("\tflareFadeSpeed: {0}", something.RenderSettings.flareFadeSpeed);
-			Console.WriteLine ("\tflareStrength: {0}", something.RenderSettings.flareStrength);
-			Console.WriteLine ("\tfog: {0}", something.RenderSettings.fog);
-			Console.WriteLine ("\tfogColor: {0}", something.RenderSettings.fogColor);
-			Console.WriteLine ("\tfogDensity: {0}", something.RenderSettings.fogDensity);
-			Console.WriteLine ("\tfogEndDistance: {0}", something.RenderSettings.fogEndDistance);
-			Console.WriteLine ("\tfogStartDistance: {0}", something.RenderSettings.fogStartDistance);
-			Console.WriteLine ("\thaloStrength: {0}", something.RenderSettings.haloStrength);
+		public void Load (string filename)
+		{
+			using (var stream = File.OpenText (filename))
+			{
+				var reader = new EventReader (new Parser (stream));
+				var deserializer = CreateDeserializer();
+
+				reader.Expect<StreamStart> ();
+				while (!reader.Accept<StreamEnd>())
+				{
+					reader.Expect<DocumentStart>();
+
+					var nodeEvent = reader.Peek<NodeEvent>();
+					var tag = nodeEvent.Tag;
+
+					if (tagMapping.ContainsKey(tag))
+					{
+						var thing = deserializer.Deserialize<Disunity.UnityEngine.Object> (reader);
+
+						Console.WriteLine("Thing: {0}", thing);
+					}
+					else
+					{
+						Console.WriteLine("Skipping tag: {0}", tag);
+
+						reader.SkipThisAndNestedEvents();
+					}
+
+					reader.Allow<DocumentEnd>();
+				}
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			var loader = new Loader();
+
+			loader.Load(args[0]);
 		}
 	}
 }
